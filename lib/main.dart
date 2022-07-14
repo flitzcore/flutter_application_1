e6e346be8b5356b4dcedc034c0e8a755baa48655
flitzcore <hemistergt@gmail.com>
2022-07-14 21:30:12 +0700

    commit1

--- lib/main.dart
+++ lib/main.dart
@@ -37,20 +37,44 @@ class DetailScreen extends StatelessWidget {
               Container(
                 margin: const EdgeInsets.symmetric(vertical: 16.0),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Column(
                       children: const <Widget>[
                         Icon(Icons.calendar_today),
+                        SizedBox(
+                          height: 5,
+                        ),
                         Text('Open Everyday'),
                       ],
                     ),
                     Column(
                       children: const <Widget>[
                         Icon(Icons.access_time_outlined),
+                        SizedBox(
+                          height: 5,
+                        ),
                         Text('09:00 - 20:00'),
+                      ],
+                    ),
+                    Column(
+                      children: const <Widget>[
+                        Icon(Icons.monetization_on),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Rp 25.000'),
                       ],
                     )
                   ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
                 ),
               ),
             ]),
